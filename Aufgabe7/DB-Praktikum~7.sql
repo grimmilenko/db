4290b63da36e48feb59b5b05b080d6bba95e4161
Nico Grimm <nico.grimm@haw-hamburg.de>
2016-01-21 08:39:37 +0100

    update

--- Aufgabe7/DB-Praktikum~7.sql
+++ Aufgabe7/DB-Praktikum~7.sql
@@ -1,34 +1,31 @@
 /* --- Aufgabenblatt 7 - Aufgabe 7.1 --- */
 
-/* View ist eine Art von SELECT (Keine Speicherung von Daten, nur Darstellung)
-	 TABLE ist einer permanente Speicherung der Daten in der Datenbank */
-
-/**/
+/* VIEW, da Daten schon vorhanden sind */
 CREATE VIEW Maschinen_Einkauf AS
 SELECT MNR, ANSCH_DATUM
 FROM MASCHINE
-WHERE ANSCH_DATUM > 31.12.2015 AND ANSCH_DATUM < 1.1.2017;
+WHERE ANSCH_DATUM > 31-DEC-2015 AND ANSCH_DATUM < 1-JAN-2017;
 
-/**/
+/* VIEW, da Daten schon vorhanden sind */
 CREATE VIEW Maschinen_Reparatur_Aktuell AS
 SELECT MNR, Rep_Erklaerung
 FROM Maschinen_Reparatur;
 
-/**/
+/* VIEW, da Daten schon vorhanden sind */
 CREATE VIEW Maschinen_Rep_Kosten_Gesamt AS
 SELECT MNR,SUM(Rep_Kosten)
 AS RepKostenGesamt
 FROM Maschinen_Reparatur
 GROUP BY MNR;
 
-/**/
+/* Datensatz so nicht vorhanden, deswegen TABLE erstellen */
 CREATE TABLE ALTERSVORSORGE (
 PNR INT,
 VorsorgeID INT PRIMARY KEY,
 FOREIGN KEY (PNR) REFERENCES Personal(PNR)
 );
 
-/**/
+/* VIEW, da Daten schon vorhanden sind */
 CREATE VIEW ABT_VERKAUF AS
 SELECT Personal.*
 FROM Personal
@@ -36,7 +33,7 @@ JOIN ABTEILUNG
 ON Personal.ABT_NR = ABTEILUNG.ABT_NR
 WHERE Abteilung.NAME = 'Verkauf';
 
-/**/
+/* Datensatz so nicht vorhanden, deswegen TABLE erstellen */
 CREATE TABLE ABTEILUNGSLEITER (
 AbtLeiterID INT PRIMARY KEY,
 PNR INT,
@@ -45,7 +42,7 @@ FOREIGN KEY (PNR) REFERENCES Personal(PNR),
 FOREIGN KEY (ABT_NR) REFERENCES Abteilung(ABT_NR)
 );
 
-/**/
+/* Datensatz so nicht vorhanden, deswegen TABLE erstellen */
 CREATE TABLE PERSONAL_ADRESSE (
 PNR INT PRIMARY KEY,
 STRASSE VARCHAR(40),
